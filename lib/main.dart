273bf5372ac899464ad1f478b80214d6eee194fb
piyushkarmacharya <piyushkarmacharya65@gmail.com>
2024-07-29 08:44:34 +0545

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import "package:flutter/material.dart";
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Ecommerce",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Ecommerce"),
+      ),
+      body: Center(child: Text("Lets build Ecommerce")),
+    );
+  }
+}
