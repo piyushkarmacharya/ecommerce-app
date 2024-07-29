67a3e7cf22f14af5f9b01ae8ecd628a4d0121367
piyushkarmacharya <piyushkarmacharya65@gmail.com>
2024-07-29 18:57:55 +0545

    create model for the products

---
+++ lib/models/product.dart
@@ -0,0 +1,12 @@
+class Product {
+  final String id, title, description, imageUrl;
+  final double price;
+  bool isFavorite;
+  Product(
+      {required this.id,
+      required this.title,
+      required this.description,
+      required this.imageUrl,
+      required this.price,
+      required this.isFavorite});
+}
