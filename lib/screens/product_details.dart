54d26b74dc2194c7a3602e0b9a2ccc7a42d39e3a
piyushkarmacharya <piyushkarmacharya65@gmail.com>
2024-08-13 20:30:37 +0545

    access the product detail from product provider using id for productDetail page

--- lib/screens/product_details.dart
+++ lib/screens/product_details.dart
@@ -1,18 +1,24 @@
+import 'package:ecommerce_app/stateManagement/providers/product_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ProductDetails extends StatelessWidget {
+class ProductDetails extends ConsumerWidget {
   static String urlName = "/product-details";
 
   const ProductDetails({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final String id = ModalRoute.of(context)!.settings.arguments as String;
+    final loadedProduct =
+        ref.watch(productProvider).firstWhere((prod) => prod.id == id);
     return Scaffold(
       appBar: AppBar(
         title: Text(id),
       ),
-      body: Container(),
+      body: Container(
+        child: Text(loadedProduct.title),
+      ),
     );
   }
 }
