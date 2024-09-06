2d0fe26e6208d91ffd94af4551720b1fa460ce8a
piyushkarmacharya <piyushkarmacharya65@gmail.com>
2024-09-06 19:54:45 +0545

    feat: view product card in cart page

--- lib/screens/cart_page.dart
+++ lib/screens/cart_page.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_app/stateManagement/providers/cart_provider.dart';
+import 'package:ecommerce_app/widgets/product_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -8,21 +9,44 @@ class CartPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final cart = ref.watch(cartProvider.notifier);
+    final cartProviderr = ref.watch(cartProvider.notifier);
+    final cart = ref.watch(cartProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Cart Page"),
       ),
-      body: Row(
-        children: [
-          const Text("Total amt:  "),
-          Chip(
-              backgroundColor: Theme.of(context).primaryColor,
-              label: Text(
-                cart.getTotalAmount().toString(),
-                style: const TextStyle(color: Colors.white),
-              )),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text("Total amt:  "),
+                Chip(
+                    backgroundColor: Theme.of(context).primaryColor,
+                    label: Text(
+                      cartProviderr.getTotalAmount().toString(),
+                      style: const TextStyle(color: Colors.white),
+                    )),
+              ],
+            ),
+            Expanded(
+              child: GridView.builder(
+                  padding: const EdgeInsets.all(10),
+                  itemCount: cart.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 3 / 3,
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 10),
+                  itemBuilder: (context, i) {
+                    return ProductItem(id: cart[i].product.id);
+                  }),
+            ),
+          ],
+        ),
       ),
     );
   }
